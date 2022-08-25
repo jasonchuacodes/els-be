15fb6ec29653f0aef03913267a106dc471a68c5e
Jason Clyde Chua <sirj@Jasons-MacBook-Air.local>
2022-08-25 15:20:29 +0800

    [ELS-001] setup registration and login api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|unique:users|regex:/(.+)@(.+)\.(.+)/i',
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = User::create([
+            'first_name' => $validated['first_name'],
+            'last_name' => $validated['last_name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password'])
+        ]);
+
+        event(new Registered($user));
+
+        return response()->json('Success! Please verify registration by clicking on the link we sent to your email.', 200);
+    }
+
+    public function login(Request $request) 
+    {
+        $validated = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        // MANUAL METHOD
+        $user = User::where('email', $request->email)->first();
+        
+        if(!$user || !Hash::check($validated['password'], $user->password)) {
+            return response()->json('Invalid user credentials. Please try again', 403);
+        } else {
+
+            $token = $user->createToken('els_token')->plainTextToken;
+            $id = $user->id;
+
+            $data = [
+                'message' => 'Successfully logged in user.',
+                'user' => $user,
+                'id' => $id,
+                'token' => $token,
+                'is_admin' => 0
+            ];
+
+            return response()->json($data);
+        }
+
+        // SHORTCUT METHOD - with Auth facade
+        // if (Auth::attempt($validated))
+        // {
+        //     $user = Auth::user();
+        //     $id = Auth::id();
+
+        //     $token = $user->createToken('els_token')->plainTextToken;
+
+        //     $data = [
+        //         'message' => 'Successfully logged in',
+        //         'user' => $user,
+        //         'id' => $id,
+        //         'token' => $token,
+        //     ];
+
+        //     return response()->json($data);
+        // } 
+        // else 
+        // {
+        //     return response()->json('Whoops! Incorrect email or password', 403);
+        // }
+    }
+
+    public function logout() 
+    {
+        $user = Auth::user();
+
+        $user->currentAccessToken()->delete();
+
+        return response()->json('Logged out user');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\VerificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+// Public Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/verify', [VerificationController::class, 'verify'])
+    ->name('verification.verify');
+Route::post('/verification-notification', [VerificationController::class, 'resend'])
+    ->middleware(['throttle:6,1'])
+    ->name('verification.send');
+
+// Private
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
